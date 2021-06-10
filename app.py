d2b7c8953206ad45f1d7ee74633f3a5cb339755e
Varun Kumar <77457158+varun-kumar-812@users.noreply.github.com>
2021-06-10 17:27:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 from flask import Flask, request, render_template
 import pickle
-import string
-import re
-from nltk import PorterStemmer, corpus
 
 app = Flask(__name__)
 model = pickle.load(open('Logistic_regression_model.pkl','rb'))
-
-def cleanText(text):
-    clean = re.compile('<.*?>')
-    ps = PorterStemmer()
-    stopwords = corpus.stopwords.words('english')
-
-    html = re.sub(clean,'',text)
-    p_text = ''.join([i.lower() for i in html if i not in string.punctuation])
-    token = re.split('\W+',p_text)
-    stop = [ps.stem(i) for i in token if i not in stopwords]
-    return stop          
-                       
 cv = pickle.load(open('Count_Vectorizer.pkl','rb'))
 
 @app.route('/', methods=['GET'])
